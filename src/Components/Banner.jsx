936650e80a7bea7e16c27d60fa90e044634ed9ff
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-02 23:18:44 +0600

    typewritter edited

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,6 +1,7 @@
 import image from "./images/profile1.jpg";
 import 'animate.css';
 import { Wave } from "react-animated-text";
+import { FaRegHandPointer } from "react-icons/fa6";
 import { FaFacebook, FaGithub, FaLinkedin, FaX } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { BsDownload } from "react-icons/bs";
@@ -8,8 +9,8 @@ import Typewriter from "typewriter-effect";
 
 const Banner = () => {
   return (
-    <section className="pt-3">
-      <div className="flex justify-center items-center">
+    <section className="pt-3 min-h-screen flex flex-col justify-center items-center">
+      <div className="flex justify-center py-10 lg:py-3 items-center">
         <img
           className="lg:w-52 lg:h-52 w-40 h-40 hover:border-textcl border-gray-500 border-2 object-cover rounded-full"
           src={image}
@@ -22,15 +23,22 @@ const Banner = () => {
           Junior Frontend Developer{" "}
           {/* <Wave effectChange={3.0} text="EXAMPLE TEXT" /> */}
         </h1>
-        <div className="text-gray-500 font-semibold text-center text-xs lg:text-sm max-w-2xl">
+        <div className="text-gray-500 min-h-[100px] lg:min-h-[90px] font-semibold text-center text-xs lg:text-sm max-w-2xl">
           <Typewriter
+          onInit={(typewriter) => {
+    typewriter
+      .typeString(
+        "I'm a passionate frontend developer skilled in crafting responsive, elegant user interfaces with React, Tailwind CSS, and JavaScript. With a strong foundation in mern-stack principles, I focus on clean code, intuitive UX, and performance optimization. I'm dedicated to building meaningful digital experiences, always learning, and ready to collaborate. Let’s bring your ideas to life efficiently, creatively, and beautifully."
+      )
+      .start();
+  }}
             options={{
-              strings: [
-                "I'm a passionate frontend developer skilled in crafting responsive, elegant user interfaces with React, Tailwind CSS, and JavaScript. With a strong foundation in mern-stack principles, I focus on clean code, intuitive UX, and performance optimization. I'm dedicated to building meaningful digital experiences, always learning, and ready to collaborate. Let’s bring your ideas to life efficiently, creatively, and beautifully.",
-              ],
+              
               autoStart: true,
-              loop: true,
-              delay: 30,
+              loop: false,
+              delay: 1,
+              deleteSpeed: 0,
+             
             }}
           />
         </div>
@@ -73,6 +81,11 @@ const Banner = () => {
             Resume <BsDownload />
           </button>
         </div>
+      </div>
+      <div className="flex justify-center w-full items-center mt-3 ">
+        <button className="btn w-full sm:w-full rounded-none hover:text-white text-gray-800 duration-300 bg-gray-900 hover:bg-textcl  shadow-none border-transparent">
+           View Portfolio <FaRegHandPointer />
+        </button>
       </div>
     </section>
   );
