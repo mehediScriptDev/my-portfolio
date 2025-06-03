89c9465334c862ac10a8337390a2979da36d7954
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-03 13:01:27 +0600

    loader added

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import loader from './images/fav.png'
 
 const Loader = () => {
     return (
-        <div>
-            
+        <div className='min-h-screen bg-maincl flex flex-col justify-center items-center'>
+            <img className='w-28 lg:w-44' src={loader} alt="" />
+            <span className="loading loading-dots bg-textcl loading-xl"></span>
         </div>
     );
 };
