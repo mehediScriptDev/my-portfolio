69d842b61a67390b4c052e7229ca65e2d7d3d28d
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-04 01:10:21 +0600

    all cleared

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,28 +1,13 @@
 import { href, Link, Outlet } from "react-router-dom";
 import mine from "./images/profile1.jpg";
-import GooeyNav from './GooeyNav'
+
 
 const Home = () => {
-    const items = [
-      { label: "Skills", href: "/portfolio" },
-      { label: "Projects", href: "/portfolio/projects" },
-      { label: "Contact", href: "/portfolio/contact" },
-    ];
+    
   return (
     <div className="text-white  overflow-hidden bg-maincl ">
-      <div className="border-b-2 flex justify-center border-white">
-        <div className="px-3 py-2  relative" >
-  <GooeyNav
-    items={items}
-    particleCount={15}
-    particleDistances={[90, 10]}
-    particleR={100}
-    initialActiveIndex={0}
-    animationTime={600}
-    timeVariance={300}
-    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-  />
-</div>
+      <div className=" flex justify-center">
+        
       </div>
 <Outlet></Outlet>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./Components/Error.jsx";
 import Home from "./Components/Home.jsx";
-import Skills from "./Components/Skills.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,14 +15,10 @@ const router = createBrowserRouter([
   {
     path: "/portfolio",
     element: <Home></Home>,
-    children:[
-{
-    index:true,
-    element: <Skills></Skills>,
+    children: [
+      
+    ],
   },
-    ]
-  },
-  
 ]);
 
 createRoot(document.getElementById("root")).render(
