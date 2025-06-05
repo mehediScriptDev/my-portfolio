5ff251e068f1b3d846b841a7c2307cba3c258122
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-05 12:21:28 +0600

    animation clearewd

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,7 +3,8 @@ import GlitchText from './GlitchText';
 const Skills = () => {
   return (
     <div className="min-h-screen">
-      <h1 className="py-2 text-2xl pb-3 font-bold text-white/80">Skills</h1>
+      <h1 className="py-2 text-2xl pb-1 font-bold text-white/80 pt-5">Skills</h1>
+      <p className='text-xs text-gray-400 pb-4'>I love diving into new tech and sharpening my development skills every day, constantly pushing myself to build better, smarter, and more meaningful digital experiences.</p>
       {/* <GlitchText
   speed={3}
   enableShadows={true}
@@ -14,7 +15,7 @@ const Skills = () => {
 </GlitchText> */}
       <div className="overflow-y-scroll pb-5">
         <div>
-          <p className="font-semibold">Language</p>
+          <p className="font-semibold text-white/80">Language</p>
           <div className="pt-5">
             <btn className="lg:py-3 lg:px-8 text-xs py-2 px-4 bg-gray-800 text-gray-400 rounded-md">
               JavaScript(ES6)
@@ -22,7 +23,7 @@ const Skills = () => {
           </div>
         </div>
         <div className="pt-6">
-          <p className="font-semibold">Frontend</p>
+          <p className="font-semibold text-white/80">Frontend</p>
           <div className="pt-2 grid grid-cols-4 gap-2">
             <btn className="lg:py-3 py-1 text-xs lg:px-8 bg-gray-800 text-gray-400 text-center rounded-md">
               Reactjs
@@ -36,7 +37,7 @@ const Skills = () => {
           </div>
         </div>
         <div className="pt-6">
-          <p className="font-semibold">Tools & Libraries</p>
+          <p className="font-semibold text-white/80">Tools & Libraries</p>
           <div className="pt-2 grid grid-cols-4 gap-2">
             <btn className="lg:py-3 py-1 text-xs flex items-center justify-center lg:px-8 bg-gray-800 text-gray-400 text-center rounded-md">
               Reactjs
@@ -74,7 +75,7 @@ const Skills = () => {
           </div>
         </div>
         <div className="pt-6">
-          <p className="font-semibold">Backend</p>
+          <p className="font-semibold text-white/80">Backend</p>
           <div className="pt-2 grid grid-cols-4 gap-2">
             <btn className="lg:py-3 py-1 text-xs flex items-center justify-center lg:px-8 bg-gray-800 text-gray-400 text-center rounded-md">
               Nodejs
@@ -88,7 +89,7 @@ const Skills = () => {
           </div>
         </div>
         <div className="pt-6">
-          <p className="font-semibold">Database & Hosting</p>
+          <p className="font-semibold text-white/80">Database & Hosting</p>
           <div className="pt-2 grid grid-cols-4 gap-2">
             <btn className="lg:py-3 py-1 text-xs flex items-center justify-center lg:px-8 bg-gray-800 text-gray-400 text-center rounded-md">
               MongoDB
